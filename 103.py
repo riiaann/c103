9f8b8241c04019812eb84fe169a278d201041bcd
riiaann <106594117+riiaann@users.noreply.github.com>
2023-11-23 18:40:29 +0530

    Add files via upload

---
+++ 103.py
@@ -0,0 +1,41 @@
+import sys
+import time
+import random
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/acer/Downloads"
+
+class FileEventHandler(FileSystemEventHandler):
+     def on_created(self, event):
+          print(f"Hey,{event.src_path} has been created!")
+
+     def on_deleted(self,event):
+          print(f"oops! someone deleted {event.src_path}")
+
+     def on_moved(self,event):
+          print(f"{event.src_path} has been moved")
+
+     def on_modified(self,event):
+          print(f"{event.src_path} has been modified")
+          
+
+event_handler = FileEventHandler()
+
+observer = Observer()
+
+observer.schedule(event_handler, from_dir, recursive=True)
+
+observer.start()
+
+try:
+ while True:
+    time.sleep(2)
+    print("running...")
+except KeyboardInterrupt:
+   print("stopped")
+   observer.stop()
+ 
